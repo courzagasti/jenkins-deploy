6e9e971e585f8343798f2bc1c378cb08c8645cd3
courzagasti <courzagasti@teco.com.ar>
2025-07-31 15:37:22 -0300

    Add terraform files

---
+++ terraform/infra/main.tf
@@ -0,0 +1,33 @@
+provider "azurerm" {
+  features {}
+  
+  subscription_id = "4dc63939-80f6-4f50-bd19-bc605cf2786d"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
+
+resource "azurerm_log_analytics_workspace" "log" {
+  name                = "${var.containerapp_name}-log"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_container_app_environment" "env" {
+  name                       = "${var.containerapp_name}-env"
+  location                   = var.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.log.id
+}
---
+++ terraform/infra/variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  default = "eastus"
+}
+
+variable "resource_group_name" {
+  default = "rg-containerapp"
+}
+
+variable "acr_name" {
+  default = "acrtfexample"
+}
+
+variable "containerapp_name" {
+  default = "my-containerapp"
+}
